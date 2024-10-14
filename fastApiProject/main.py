5ee42442356678149fd90b19089ce30ee08d7244
Vinicius Hartmann Ferreira <vinihf@gmail.com>
2024-10-14 14:59:17 -0300

    Novidades

--- fastApiProject/main.py
+++ fastApiProject/main.py
@@ -40,6 +40,7 @@ async def say_hello(name: str):
 from fastapi import FastAPI
 from typing import Optional
 from sqlmodel import Field, SQLModel, Relationship, create_engine, Session, select
+from sqlalchemy.orm import joinedload
 
 class Pokemon(SQLModel, table=True):
     Pokedex_number: Optional[int] = Field(default=None, primary_key=True)
@@ -47,7 +48,7 @@ class Pokemon(SQLModel, table=True):
     is_legendary: int
     Defense: int
     Attack: int
-    type_id: int = Field(foreign_key="type.id_type")  # Renomear para evitar conflito
+    type_id: int = Field(foreign_key="type.id_type")
     type_all: "Type" = Relationship(back_populates="pokemons")
 
 class Type(SQLModel, table=True):
@@ -61,14 +62,19 @@ def get_all():
     with Session(engine) as session:
         statement = select(Pokemon)
         results = session.exec(statement)
-        return results.all()  # Corrigir para retornar uma lista
+        return results.all()
 
+def get_id(id:int):
+    with Session(engine) as session:
+        statement = select(Pokemon).options(joinedload(Pokemon.type_all)).where(Pokemon.Pokedex_number == id)
+        result = session.exec(statement)
+        return result.one()
 app = FastAPI()
 
-@app.get("/", response_model=list[Pokemon])
+@app.get("/")
 async def root():
     return get_all()  # Corrigir para retornar o valor da função
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/{id:int}")
+async def root_get(id: int):
+    return get_id(id)
